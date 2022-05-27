8a33fa5a20b5629f70eaa387295b518b8ea2a2bd
kineticvenom <kalebvarnes@yahoo.com>
2022-05-27 12:26:23 -1000

    Code Works!

--- js/characterMatch.js
+++ js/characterMatch.js
@@ -1,3 +1,34 @@
 exports.isCharacterMatch = function(string1, string2) {
+    let cnt1 = 0
+    let str1Lower = string1.replace(/[^\w]/g, "").toLowerCase()
+    let str2Lower = string2.replace(/[^\w]/g, "").toLowerCase()
+    // regexp - (/[a-zA-Z]/)
+    // console.log(str2Lower)
+    let arrSplit = str1Lower.split("")
+    let cnt2 = str2Lower.length
+
+    //  console.log(arrSplit)
+    if (cnt2 === str1Lower.length) {    
+        for (i in arrSplit) {
+            //  console.log(arrSplit[i])
+            if (arrSplit.includes(str2Lower[i])) {
+                cnt1++;
+            }
+        }
+    }
+    if(cnt1 === cnt2) {
+        return true
+    } return false
+    
+
 
 };
+
+
+// console.log(ana.isCharacterMatch('charm', 'march') === true);
+// console.log(ana.isCharacterMatch('attapp', 'attack') === false);
+// console.log(ana.isCharacterMatch('CharM', 'mARcH') === true);
+// console.log(ana.isCharacterMatch('abcde2', 'c2abed') === true);
+
+// console.log("This test is for the challenge quesiton");
+// console.log(ana.isCharacterMatch('Anna Madrigal', 'A man and a girl') === true);
--- js/characterMatchSpec.js
+++ js/characterMatchSpec.js
@@ -3,7 +3,7 @@
 var ana = require("./characterMatch");
 
 console.log(ana.isCharacterMatch('charm', 'march') === true);
-console.log(ana.isCharacterMatch('zach', 'attack') === false);
+console.log(ana.isCharacterMatch('attapp', 'attack') === false);
 console.log(ana.isCharacterMatch('CharM', 'mARcH') === true);
 console.log(ana.isCharacterMatch('abcde2', 'c2abed') === true);
 
